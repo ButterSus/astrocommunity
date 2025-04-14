eac64c65d6eee877445035c1eece4d2ec2a08665
Krivoshapkin Eduard <eduardkrivosapkin5@gmail.com>
2025-04-14 15:00:06 +0300

    custom(mini-surround): override mappings to vim-surround style

--- lua/astrocommunity/motion/mini-surround/init.lua
+++ lua/astrocommunity/motion/mini-surround/init.lua
@@ -1,43 +1,35 @@
-local prefix = "gz"
 return {
   "echasnovski/mini.surround",
   dependencies = {
     { "AstroNvim/astroui", opts = { icons = { Surround = "󰑤" } } },
-    {
-      "AstroNvim/astrocore",
-      opts = function(_, opts)
-        local maps = opts.mappings
-        maps.n[prefix] = { desc = require("astroui").get_icon("Surround", 1, true) .. "Surround" }
-      end,
-    },
+    { "AstroNvim/astrocore" },
   },
-  keys = function(_, keys)
-    local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
-    local opts = require("lazy.core.plugin").values(plugin, "opts", false) -- resolve mini.clue options
-    -- Populate the keys based on the user's options
-    local mappings = {
-      { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
-      { opts.mappings.delete, desc = "Delete surrounding" },
-      { opts.mappings.find, desc = "Find right surrounding" },
-      { opts.mappings.find_left, desc = "Find left surrounding" },
-      { opts.mappings.highlight, desc = "Highlight surrounding" },
-      { opts.mappings.replace, desc = "Replace surrounding" },
-      { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
-    }
-    mappings = vim.tbl_filter(function(m) return m[1] and #m[1] > 0 end, mappings)
-    return vim.list_extend(mappings, keys)
-  end,
   opts = {
+    -- Setup similar to 'tpope/vim-surround'
     mappings = {
-      add = prefix .. "a", -- Add surrounding in Normal and Visual modes
-      delete = prefix .. "d", -- Delete surrounding
-      find = prefix .. "f", -- Find surrounding (to the right)
-      find_left = prefix .. "F", -- Find surrounding (to the left)
-      highlight = prefix .. "h", -- Highlight surrounding
-      replace = prefix .. "r", -- Replace surrounding
-      update_n_lines = prefix .. "n", -- Update `n_lines`
+      add = "ys", -- Add surrounding in Normal and Visual modes
+      delete = "ds", -- Delete surrounding
+      find = "", -- Find surrounding (to the right)
+      find_left = "", -- Find surrounding (to the left)
+      highlight = "", -- Highlight surrounding
+      replace = "cs", -- Replace surrounding
+      update_n_lines = "", -- Update `n_lines`
+
+      -- Add this only if you don't want to use extended mappings
+      suffix_last = "",
+      suffix_next = "",
     },
   },
+  config = function(_, opts)
+    require("mini.surround").setup(opts)
+
+    -- Remap adding surrounding to Visual mode selection
+    vim.keymap.del("x", "ys")
+    vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+
+    -- Make special mapping for "add surrounding for line"
+    vim.keymap.set("n", "yss", "ys_", { remap = true })
+  end,
   specs = {
     {
       "catppuccin",
