32c7f458a130d438ef622de949a146237dc44720
Krivoshapkin Eduard <eduardkrivosapkin5@gmail.com>
2025-04-17 09:34:07 +0300

    feat(mini-files): add set_cwd, yank_path mappings

--- lua/astrocommunity/file-explorer/mini-files/init.lua
+++ lua/astrocommunity/file-explorer/mini-files/init.lua
@@ -44,6 +44,35 @@ return {
       },
     },
   },
+  config = function()
+    -- Source: https://github.com/echasnovski/mini.nvim/blob/12ebac8280869048c286d13bd35cfa6653e07642/doc/mini-files.txt#L508-L534
+    local MiniFiles = require "mini.files"
+
+    -- Set focused directory as current working directory
+    local set_cwd = function()
+      local path = (MiniFiles.get_fs_entry() or {}).path
+      if path == nil then return vim.notify "Cursor is not on valid entry" end
+      vim.fn.chdir(vim.fs.dirname(path))
+    end
+
+    -- Yank in register full path of entry under cursor
+    local yank_path = function()
+      local path = (MiniFiles.get_fs_entry() or {}).path
+      if path == nil then return vim.notify "Cursor is not on valid entry" end
+      vim.fn.setreg(vim.v.register, path)
+    end
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesBufferCreate",
+      callback = function(args)
+        local b = args.data.buf_id
+        vim.keymap.set("n", ".", set_cwd, { buffer = b, desc = "Set cwd" })
+        vim.keymap.set("n", "gy", yank_path, { buffer = b, desc = "Yank path" })
+      end,
+    })
+
+    MiniFiles.setup(opts)
+  end,
   specs = {
     { "neo-tree.nvim", optional = true, enabled = false },
     {
