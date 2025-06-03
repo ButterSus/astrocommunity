8c81f5c31ba076459aa6fe7460d9280524bf92b9
Krivoshapkin Eduard <buttersus@mail.ru>
2025-06-03 11:58:24 +0300

    custom(harpoon): add snacks picker, use custom mappings

--- lua/astrocommunity/motion/harpoon/init.lua
+++ lua/astrocommunity/motion/harpoon/init.lua
@@ -4,17 +4,13 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope.nvim", optional = true },
-    { "AstroNvim/astroui", opts = { icons = { Harpoon = "󱡀" } } },
     {
       "AstroNvim/astrocore",
       opts = function(_, opts)
         local maps = opts.mappings
-        local term_string = vim.fn.exists "$TMUX" == 1 and "tmux" or "term"
-        local prefix = "<Leader><Leader>"
-        maps.n[prefix] = { desc = require("astroui").get_icon("Harpoon", 1, true) .. "Harpoon" }
 
-        maps.n[prefix .. "a"] = { function() require("harpoon"):list():add() end, desc = "Add file" }
-        maps.n[prefix .. "e"] = {
+        maps.n["<Leader>a"] = { function() require("harpoon"):list():add() end, desc = "Add file" }
+        maps.n["<Leader><Leader>"] = {
           function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end,
           desc = "Toggle quick menu",
         }
@@ -30,17 +26,58 @@ return {
         maps.n["<C-p>"] = { function() require("harpoon"):list():prev() end, desc = "Goto previous mark" }
         maps.n["<C-n>"] = { function() require("harpoon"):list():next() end, desc = "Goto next mark" }
         if require("astrocore").is_available "telescope.nvim" then
-          maps.n[prefix .. "m"] = { "<Cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" }
+          maps.n["<Leader>fi"] = { "<Cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" }
+        elseif require("astrocore").is_available "snacks.nvim" then
+          -- https://github.com/folke/snacks.nvim/discussions/1058#discussioncomment-12450702
+          local normalize_list = function(t)
+            local normalized = {}
+            for _, v in pairs(t) do
+              if v ~= nil then table.insert(normalized, v) end
+            end
+            return normalized
+          end
+          local harpoon = require "harpoon"
+          maps.n["<leader>fi"] = {
+            function()
+              require("snacks").picker {
+                finder = function()
+                  local file_paths = {}
+                  local list = normalize_list(harpoon:list().items)
+                  for _, item in ipairs(list) do
+                    table.insert(file_paths, { text = item.value, file = item.value })
+                  end
+                  return file_paths
+                end,
+                win = {
+                  input = {
+                    keys = { ["dd"] = { "harpoon_delete", mode = { "n", "x" } } },
+                  },
+                  list = {
+                    keys = { ["dd"] = { "harpoon_delete", mode = { "n", "x" } } },
+                  },
+                },
+                actions = {
+                  harpoon_delete = function(picker, item)
+                    local to_remove = item or picker:selected()
+                    harpoon:list():remove { value = to_remove.text }
+                    harpoon:list().items = normalize_list(harpoon:list().items)
+                    picker:find { refresh = true }
+                  end,
+                },
+              }
+            end,
+            desc = "Find marks (harpoon)",
+          }
         end
-        maps.n[prefix .. "t"] = {
-          function()
-            vim.ui.input({ prompt = term_string .. " window number: " }, function(input)
-              local num = tonumber(input)
-              if num then require("harpoon").term.gotoTerminal(num) end
-            end)
-          end,
-          desc = "Go to " .. term_string .. " window",
-        }
+        -- maps.n[prefix .. "t"] = {
+        --   function()
+        --     vim.ui.input({ prompt = term_string .. " window number: " }, function(input)
+        --       local num = tonumber(input)
+        --       if num then require("harpoon").term.gotoTerminal(num) end
+        --     end)
+        --   end,
+        --   desc = "Go to " .. term_string .. " window",
+        -- }
       end,
     },
   },
