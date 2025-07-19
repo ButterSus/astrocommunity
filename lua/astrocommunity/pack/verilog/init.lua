9ab7613568dde7390dffe5ed018570f46e1b022e
Krivoshapkin Eduard <buttersus@mail.ru>
2025-07-20 03:00:42 +0900

    feat(verilog): add verilator to none-ls

--- lua/astrocommunity/pack/verilog/init.lua
+++ lua/astrocommunity/pack/verilog/init.lua
@@ -22,4 +22,27 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "verible" })
     end,
   },
+  {
+    "mfussenegger/nvim-lint",
+    config = function()
+      local lint = require "lint"
+
+      -- Configure linters by filetype
+      lint.linters_by_ft = {
+        systemverilog = { "verilator" },
+        verilog = { "verilator" },
+      }
+
+      -- Optional: Customize verilator arguments
+      lint.linters.verilator.args = {
+        "--lint-only",
+        "--Wall",
+      }
+
+      -- Auto-lint on these events
+      vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+        callback = function() lint.try_lint() end,
+      })
+    end,
+  },
 }
